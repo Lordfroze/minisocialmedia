ac68d51dd46e97e21f0f3da31b38da978b038847
froze <froze@debian>
2025-02-17 19:31:14 +0700

    update

---
+++ minisocialmedia/database/seeders/LikesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // untuk menggunakan query builder
+
+class LikesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('likes')->insert( [
+            [
+                'user_id' => 1,
+                'post_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => 1,
+                'post_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+            ]);
+
+    }
+}
